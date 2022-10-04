8b9427c28c23ad4e37cd436c6d5557d006abf8ca
Alex-Verdugo <73000648+Alex-Verdugo@users.noreply.github.com>
2022-10-04 14:14:10 -0700

    Test commit

--- src/components/AddAssignment.js
+++ src/components/AddAssignment.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import {DataGrid} from '@mui/x-data-grid';
+import Cookies from 'js-cookie';
 // import {DataGrid} from '@mui/x-data-grid';
 // import Cookies from 'js-cookie';
 // import {SERVER_URL} from '../constants.js';
@@ -16,34 +15,35 @@ class AddAssignment extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = { assignments: [] };
       };
 
     // when submit button pressed, send new assignment to back end 
-    // handleSubmit = ( ) => {
-    //     console.log("AddAssignment.handleSubmit");
-    //     const token = Cookies.get('XSRF-TOKEN');
+    handleSubmit = ( ) => {
+        console.log("AddAssignment.handleSubmit");
+        const token = Cookies.get('XSRF-TOKEN');
         
-    //     fetch(`http://localhost:8081//assignment/createAssignment?AssignmentName={} 3&AssignmentDueDate={}&CourseId={}` , 
-    //         {  
-    //           method: 'POST', 
-    //           headers: { 'Content-Type': 'application/json',
-    //                      'X-XSRF-TOKEN': token }, 
-    //           body: JSON.stringify({assignmentId:this.props.location.assignment.assignmentId,  grades: this.state.grades})
-    //         } )
-    //     .then(res => {
-    //         if (res.ok) {
-    //           toast.success("Assignment successfully Created", {
-    //           position: toast.POSITION.BOTTOM_LEFT
-    //           });
+        fetch(`http://localhost:8081//assignment/createAssignment?AssignmentName=${this.state.AssignmentName}&AssignmentDueDate=${this.state.AssignmentDueDate}&CourseId=${this.state.AssignmentCourseID}` , 
+            {  
+              method: 'POST', 
+              headers: { 'Content-Type': 'application/json',
+                         'X-XSRF-TOKEN': token }, 
+              body: JSON.stringify({assignmentId:this.props.location.assignment.assignmentId,  assignments: this.state.assignments})
+            } )
+        .then(res => {
+            if (res.ok) {
+              toast.success("Assignment successfully Created", {
+              position: toast.POSITION.BOTTOM_LEFT
+              });
             
-    //         } })
-    //       .catch(err => {
-    //         toast.error("Failed to Create Assignment", {
-    //           position: toast.POSITION.BOTTOM_LEFT
-    //         });
-    //         console.error(err);
-    //       });
-    //  };        
+            } })
+          .catch(err => {
+            toast.error("Failed to Create Assignment", {
+              position: toast.POSITION.BOTTOM_LEFT
+            });
+            console.error(err);
+          });
+     };        
 
     // when user has entered a new grade, update the state
     //  id    - index of row of grade change
@@ -76,27 +76,47 @@ class AddAssignment extends React.Component {
 
     render() {
         const columns = [
-         { field: 'assignmentName', headerName: 'Assignment Name', width: 250, 
-        //  renderCell: (params) => (
-        //     <div contentEditable = "true">
-            
-        //     </div>
-        //   )
-        
-        },
-         { field: 'dueDate', headerName: 'Due Date', width: 250},
+         { field: 'assignmentName', headerName: 'Assignment Name', width: 250, editable:true },
+         { field: 'dueDate', headerName: 'Due Date', width: 250, editable:true},
          ];
          
+         const CourseId = this.props.location.CourseId;
+
          return(
              <div align="left" >
                 <h4>Enter Assignment Feilds </h4>
-               <div style={{ height: 400, width: '100%' }}>
-                    <DataGrid columns={columns} />
-                 <Button id="CreateAssignment" variant="outlined" color="primary" style={{margin: 10}} onClick={this.handlecreate} >
+               <div style={{ height: 250, width: '100%' ,margin:20}}>
+
+               
+               <div  style={{display: "flex",width: '70%',margin:20,justifyContent:"space-between"}}>
+               <h5>Assignment Name: </h5>
+               <input
+                    type= "text"
+                    id="AssignmentName"
+                    onChange={this.handleChange}
+                    value={this.state.AssignmentName}
+                    />
+                     <h5>Assignment Due Date: </h5>
+               <input
+                    type= "text"
+                    id="AssignmentDueDate"
+                    onChange={this.handleChange}
+                    value={this.state.AssignmentDueDate}
+                    />
+                     <h5>Assignment Course ID: </h5>
+               <input
+                    type= "text"
+                    id="AssignmentCourseID"
+                    onChange={this.handleChange}
+                    value={this.state.AssignmentCourseID}
+
+                    />
+                </div>
+               </div>
+                <Button id="CreateAssignment" variant="outlined" color="primary" onClick={this.handleSubmit} >
                     Create Assignment
                  </Button>
-               </div>
-               <ToastContainer autoClose={1500} />   
+              
              </div>
              ); 
          }
