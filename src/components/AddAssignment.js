82581c5d872cea25bf8a94cf0b5fd106344ff72a
Alex-Verdugo <73000648+Alex-Verdugo@users.noreply.github.com>
2022-10-04 16:35:22 -0700

    Completed Add Assignment.js
    Fixed URL to have params and reorganized methods, changed from text inputs to submit form

--- src/components/AddAssignment.js
+++ src/components/AddAssignment.js
@@ -1,125 +1,102 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Button from '@mui/material/Button';
 import Cookies from 'js-cookie';
-// import {DataGrid} from '@mui/x-data-grid';
-// import Cookies from 'js-cookie';
-// import {SERVER_URL} from '../constants.js';
 
-// NOTE:  for OAuth security, http request must have
-//   credentials: 'include' 
-//
 
-class AddAssignment extends React.Component {
 
+class AddAssignment extends Component {
     constructor(props) {
-        super(props);
-        this.state = { assignments: [] };
-      };
-
-    // when submit button pressed, send new assignment to back end 
-    handleSubmit = ( ) => {
-        console.log("AddAssignment.handleSubmit");
-        const token = Cookies.get('XSRF-TOKEN');
-        
-        fetch(`http://localhost:8081//assignment/createAssignment?AssignmentName=${this.state.AssignmentName}&AssignmentDueDate=${this.state.AssignmentDueDate}&CourseId=${this.state.AssignmentCourseID}` , 
-            {  
-              method: 'POST', 
-              headers: { 'Content-Type': 'application/json',
-                         'X-XSRF-TOKEN': token }, 
-              body: JSON.stringify({assignmentId:this.props.location.assignment.assignmentId,  assignments: this.state.assignments})
-            } )
-        .then(res => {
-            if (res.ok) {
-              toast.success("Assignment successfully Created", {
-              position: toast.POSITION.BOTTOM_LEFT
-              });
-            
-            } })
-          .catch(err => {
-            toast.error("Failed to Create Assignment", {
-              position: toast.POSITION.BOTTOM_LEFT
-            });
-            console.error(err);
-          });
-     };        
-
-    // when user has entered a new grade, update the state
-    //  id    - index of row of grade change
-    //  props - contains the new grade
-    handleEditCellChange = ({ id, field, props }) => {
-        console.log("edit cell change id:"+id+" field:"+field+" value:"+props.value);
-        const newgrades = this.state.grades.map(r => {
-          if (r.id === id){
-            return {...r, [field]:props.value};
-          } else {
-            return {...r};
-          }
-        });
-        this.setState({grades: newgrades});
-      };
- 
-     handleCellEditCommit = (e) => {
-       console.log("handleCellEditCommit "+JSON.stringify(e));
-       const newgrades= this.state.grades.map(r => {
-         //console.log(r.id+"   "+e.id);
-         if (r.id === e.id) {
-           return {...r, [e.field]:e.value};
-         } else {
-           return {...r};
-         }
-       });
-       this.setState({grades: newgrades});
-     };
+      super(props);
+      this.state = {courseId: '', assignmentName: '', dueDate: '' };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    };
   
 
-    render() {
-        const columns = [
-         { field: 'assignmentName', headerName: 'Assignment Name', width: 250, editable:true },
-         { field: 'dueDate', headerName: 'Due Date', width: 250, editable:true},
-         ];
-         
-         const CourseId = this.props.location.CourseId;
+    //gets values from form
+    handleChange(event) {
+      const target = event.target;
+      const value = target.value;
+      const name = target.name;
+      this.setState({
+        [name]: value
+      });
+    }
 
-         return(
-             <div align="left" >
-                <h4>Enter Assignment Feilds </h4>
-               <div style={{ height: 250, width: '100%' ,margin:20}}>
 
-               
-               <div  style={{display: "flex",width: '70%',margin:20,justifyContent:"space-between"}}>
-               <h5>Assignment Name: </h5>
-               <input
-                    type= "text"
-                    id="AssignmentName"
-                    onChange={this.handleChange}
-                    value={this.state.AssignmentName}
-                    />
-                     <h5>Assignment Due Date: </h5>
-               <input
-                    type= "text"
-                    id="AssignmentDueDate"
-                    onChange={this.handleChange}
-                    value={this.state.AssignmentDueDate}
-                    />
-                     <h5>Assignment Course ID: </h5>
-               <input
-                    type= "text"
-                    id="AssignmentCourseID"
-                    onChange={this.handleChange}
-                    value={this.state.AssignmentCourseID}
+ 
+  handleSubmit(event){
+      event.preventDefault();
+      console.log("Adding New Assignment");
+      console.log(JSON.stringify({courseId:this.state.courseId, assignmentName:this.state.assignmentName, dueDate:this.state.dueDate}));
+      const token = Cookies.get('XSRF-TOKEN');
+      
+      //putting params in URL because I used Get Requests in backend method 
+      fetch(`http://localhost:8081//assignment/createAssignment?AssignmentName=${this.state.assignmentName} 3&AssignmentDueDate=${this.state.dueDate}&CourseId=${this.state.courseId}` , 
+          {  
+            method: 'POST', 
+            headers: { 'Content-Type': 'application/json',
+                       'X-XSRF-TOKEN': token }, 
+            body: JSON.stringify({courseId:this.state.courseId, assignmentName:this.state.assignmentName, dueDate:this.state.dueDate})
+          } )
+      .then(res => {
+          if (res.ok) {
+            toast.success("Assignment successfully created", {
+            position: toast.POSITION.BOTTOM_LEFT
+            });
+            this.setState({courseId: '', assignmentName: '', dueDate: '' });
+          } else {
+            console.log(res.body);
+            toast.error("Assignment creation failed 1", {
+            position: toast.POSITION.BOTTOM_LEFT
+            });
+            console.error('P http status =' + res.status);
+      }})
+        .catch(err => {
+          toast.error("Assignment creation failed 2", {
+            position: toast.POSITION.BOTTOM_LEFT
+          });
+          console.error(err);
+        });
+  } 
+ 
+  //creating form to input assignment details
+  render() {
+      return (
+          <div align="left" >
+                <h4>Create a new assignment: </h4>
+                  <form onSubmit={this.handleSubmit}>
+                    
+                    <label>
+                      Assignment Name:
+                    </label>
+                    <p>
+                      <input type="text" name="assignmentName" value={this.state.assignmentName} onChange={this.handleChange}/>
+                    </p>
+                    <label>
+                      Due Date:
+                    </label>
+                    <p>
+                      <input type="text" name="dueDate" value={this.state.dueDate} onChange={this.handleChange}/>
+                    </p>
+                    <label>
+                      Course Id:
+                    </label>
+                    <p>
+                        {/* Might needs to find way to pull Course Id from session */}
+                      <input type="text" name="courseId" value={this.state.courseId} onChange={this.handleChange}/>
+                    </p>
+                    <p>
+                    <input type="submit" value="Submit" />
+                    </p>
+                    <div>
+                    <ToastContainer autoClose={1500} />   
+                  </div>
+                  </form>
+          </div>
+      )
+  }
+}  
 
-                    />
-                </div>
-               </div>
-                <Button id="CreateAssignment" variant="outlined" color="primary" onClick={this.handleSubmit} >
-                    Create Assignment
-                 </Button>
-              
-             </div>
-             ); 
-         }
-
-}
 export default AddAssignment;
